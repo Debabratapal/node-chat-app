d21b021a92080407181770aee53c84afb4a4f6bf
Debabratapal <paldebabrata82@gmail.com>
2018-05-09 15:48:23 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000 || process.env.PORT;
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+app.listen(port, () => {
+  console.log('server is up @', port);
+})
