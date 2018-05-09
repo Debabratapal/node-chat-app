7fcb3373c5656738e156514c1846b446c028260c
Debabratapal <paldebabrata82@gmail.com>
2018-05-09 18:52:25 +0530

    emit message added

--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const app = express();
+var server = http.createServer(app);
 const port =process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../public')))
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('new User connected');
+
+  socket.on('createMessage' ,(message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage',{
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    })
+  })
 
 
-app.listen(port, () => {
+  socket.on('disconnect', () => {
+    console.log('user is disconnected');
+  });
+});
+
+
+
+server.listen(port, () => {
   console.log(`server is up @ ${port}`);
 })
