b270d97276111b34ced2ef3147b43f8b16921824
Debabratapal <paldebabrata82@gmail.com>
2018-05-09 20:04:01 +0530

    greetings from admin and broadcast to others

--- server/server.js
+++ server/server.js
@@ -12,6 +12,19 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('new User connected');
 
+
+  socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'welcome to chat app',
+    createAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'new user joined',
+    createAt: new Date().getTime()
+  });
+
   socket.on('createMessage' ,(message) => {
     console.log('createMessage', message);
     io.emit('newMessage',{
@@ -19,7 +32,12 @@ io.on('connection', (socket) => {
       text: message.text,
       createAt: new Date().getTime()
     })
-  })
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // });
+  });
 
 
   socket.on('disconnect', () => {
